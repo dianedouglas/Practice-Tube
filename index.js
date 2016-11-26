1e9a6d4067458327c21b7f8caa1338c7acd18eae
dianedouglas <diane.douglas1@gmail.com>
2016-11-26 01:43:04 -0500

    cleanup extra, take out extra functions used for testing. add comments.

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
+// global variable setup for video query. used with next/prev page requests.
 var searchTerm;
-function search(query, pageToken) {
-    // Use the JavaScript client library to create a search.list() API call.
-    if(pageToken) {    
-      var request = gapi.client.youtube.search.list({
-          part: 'snippet',
-          q: query,
-          pageToken: pageToken,
-          maxResults: 25
-      });
-    }else {    
-      var request = gapi.client.youtube.search.list({
-          part: 'snippet',
-          q: query,
-          maxResults: 25
-      });
-    }
 
-    
-    // Send the request to the API server,
-    // and invoke onSearchRepsonse() with the response.
-    request.execute(onSearchResponse);
+// attach click handler to search button when document ready.
+$(document).ready(function(){
+  $('#search').click(function(){
+    searchTerm = $('#find-videos').val();
+    search(searchTerm);
+  });
+});
+
+function search(query, pageToken) {
+  // Use the JavaScript client library to create a search.list() API call.
+  if(pageToken) {    
+    var request = gapi.client.youtube.search.list({
+        part: 'snippet',
+        q: query,
+        pageToken: pageToken,
+        maxResults: 25
+    });
+  }else {    
+    var request = gapi.client.youtube.search.list({
+        part: 'snippet',
+        q: query,
+        maxResults: 25
+    });
+  }
+  // Send the request to the API server,
+  // when response comes back, call 'onSearchResponse' function.
+  request.execute(onSearchResponse);
 }
 
-// Called automatically with the response of the YouTube API request.
+// Display results of search and pagers.
 function onSearchResponse(response) {
     $('#results').empty();
     response.items.forEach(function(video){
@@ -60,22 +68,11 @@ function onSearchResponse(response) {
 }
 
 (function() {
-  // Retrieve your client ID from the Google Developers Console at
-  // https://console.developers.google.com/.
+  // set up api data
   var OAUTH2_CLIENT_ID = '1070803394366-and4mdf2i6p63bql38m81klajnshep25.apps.googleusercontent.com';
   var OAUTH2_SCOPES = [
-    'https://www.googleapis.com/auth/yt-analytics.readonly',
     'https://www.googleapis.com/auth/youtube.readonly'
   ];
-
-  var ONE_MONTH_IN_MILLISECONDS = 1000 * 60 * 60 * 24 * 30;
-
-  // Keep track of the currently authenticated user's YouTube channel ID.
-  var channelId;
-
-  // For information about the Google Chart Tools API, see:
-  // https://developers.google.com/chart/interactive/docs/quick_start
-  google.load('visualization', '1.0', {'packages': ['corechart']});
 
   // Upon loading, the Google APIs JS client automatically invokes this callback.
   // See https://developers.google.com/api-client-library/javascript/features/authentication 
@@ -126,33 +123,12 @@ function onSearchResponse(response) {
     }
   }
 
-  // Load the client interfaces for the YouTube Analytics and Data APIs, which
-  // are required to use the Google APIs JS client. More info is available at
+  // Load the client interfaces for the YouTube Data API if auth successful.
   // https://developers.google.com/api-client-library/javascript/dev/dev_jscript#loading-the-client-library-and-the-api
   function loadAPIClientInterfaces() {
     gapi.client.load('youtube', 'v3', function() {
-      // gapi.client.load('youtubeAnalytics', 'v1', function() {
-        // After both client interfaces load, use the Data API to request
-        // information about the authenticated user's channel.
         search('lord of the rings');
-        // getUserChannel();
-      // });
     });
   }
 
 })();
-  // This helper method displays a message on the page.
-  function displayMessage(message) {
-    $('#message').text(message).show();
-  }
-
-  // This helper method hides a previously displayed message on the page.
-  function hideMessage() {
-    $('#message').hide();
-  }
-$(document).ready(function(){
-  $('#search').click(function(){
-    searchTerm = $('#find-videos').val();
-    search(searchTerm);
-  });
-});
