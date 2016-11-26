2b779f758bda83340ca3f34fb388e7e3232f16a8
Gibberish314 <a.moosen@gmail.com>
2016-11-26 11:42:00 -0500

    max results to 13.

--- index.js
+++ index.js
@@ -105,13 +105,13 @@ function search(query, pageToken) {
         part: 'snippet',
         q: query,
         pageToken: pageToken,
-        maxResults: 12
+        maxResults: 13
     });
   }else {    
     var request = gapi.client.youtube.search.list({
         part: 'snippet',
         q: query,
-        maxResults: 12
+        maxResults: 13
     });
   }
   // execute request, send to API server,
