a610c83f8451998166f1efb35931fc057eb80acf
dianedouglas <diane.douglas1@gmail.com>
2016-12-09 22:38:45 -0500

    test non auto auth.

--- js/index.js
+++ js/index.js
@@ -215,24 +215,24 @@ function onSearchResponse(response) {
 
   // Upon loading, the Google APIs JS client automatically invokes this callback.
   // See https://developers.google.com/api-client-library/javascript/features/authentication 
-  window.onJSClientLoad = function() {
-    gapi.auth.init(function() {
-      window.setTimeout(checkAuth, 1);
-    });
-  };
+  // window.onJSClientLoad = function() {
+  //   gapi.auth.init(function() {
+  //     window.setTimeout(checkAuth, 1);
+  //   });
+  // };
 
   // Attempt the immediate OAuth 2.0 client flow as soon as the page loads.
   // If the currently logged-in Google Account has previously authorized
   // the client specified as the OAUTH2_CLIENT_ID, then the authorization
   // succeeds with no user intervention. Otherwise, it fails and the
   // user interface that prompts for authorization needs to display.
-  function checkAuth() {
-    gapi.auth.authorize({
-      client_id: OAUTH2_CLIENT_ID,
-      scope: OAUTH2_SCOPES,
-      immediate: false
-    }, handleAuthResult);
-  }
+  // function checkAuth() {
+  //   gapi.auth.authorize({
+  //     client_id: OAUTH2_CLIENT_ID,
+  //     scope: OAUTH2_SCOPES,
+  //     immediate: false
+  //   }, handleAuthResult);
+  // }
 
   // Handle the result of a gapi.auth.authorize() call.
   function handleAuthResult(authResult) {
